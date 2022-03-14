fbef33b583f555ce4fddf7458471c8eea3fc924f
thespite <the.spite@gmail.com>
2022-03-14 00:21:53 +0000

    Update PlasticBrickGeometry.js

--- PlasticBrickGeometry.js
+++ PlasticBrickGeometry.js
@@ -9,19 +9,16 @@ function generatePlasticBrickGeometry(s, studs = 1) {
   const top = new CylinderBufferGeometry(
     s / (3.5 * studs),
     s / (3.5 * studs),
-    s / 2,
+    0.25 * s,
     18,
     1
   ).toNonIndexed();
   const geos = [block];
+  const offset = -(s / 2) + s / (2 * studs);
   for (let y = 0; y < studs; y++) {
     for (let x = 0; x < studs; x++) {
       const g = top.clone();
-      g.translate(
-        0 + (x * s) / studs - (0.5 * s) / studs,
-        s / 2,
-        0 + (y * s) / studs - (0.5 * s) / studs
-      );
+      g.translate((x * s) / studs + offset, s / 2, (y * s) / studs + offset);
       geos.push(g);
     }
   }
