d69f880c8f9d9aaf57f7e67b88c0ffa0028600fb
thespite <the.spite@gmail.com>
2022-03-21 22:22:21 +0000

    Update HeightMap.js

--- HeightMap.js
+++ HeightMap.js
@@ -14,6 +14,7 @@ import {
   MeshNormalMaterial,
   Quaternion,
   Matrix4,
+  BufferAttribute,
 } from "./third_party/three.module.js";
 import { RoundedBoxGeometry } from "./third_party/RoundedBoxGeometry.js";
 import { generateRoundedPrismGeometry } from "./RoundedPrismGeomtry.js";
@@ -43,10 +44,10 @@ class HeightMap {
     this.mode = PlasticBrick;
     // this.generateBoxGeometry();
     // this.generateRoundedBoxGeometry();
-    // this.generatePlasticBrickGeometry();
-    // this.generateGridPoints();
-    this.generateHexagonGeometry();
-    this.generateHexagonGrid();
+    this.generatePlasticBrickGeometry();
+    this.generateGridPoints();
+    // this.generateHexagonGeometry();
+    // this.generateHexagonGrid();
     this.initMesh();
     this.updatePositions();
   }
@@ -95,7 +96,7 @@ class HeightMap {
   }
 
   filter(v) {
-    // return true;
+    return true;
     return this.filterCircle(v);
     return this.filterHexagon(v);
   }
@@ -324,15 +325,29 @@ class HeightMap {
   bakePLY() {
     const exporter = new PLYExporter();
     const scene = new Scene();
-    const material = new MeshNormalMaterial();
+    const material = new MeshBasicMaterial({ vertexColors: true });
     const mat = new Matrix4();
     const position = new Vector3();
     const quaternion = new Quaternion();
     const scale = new Vector3();
+    const c = new Color();
+
     for (let i = 0; i < this.mesh.instanceMatrix.count; i++) {
       const geo = this.geo.clone();
+      geo.setAttribute(
+        "color",
+        new BufferAttribute(
+          new Float32Array(geo.attributes.position.count * 3),
+          3
+        )
+      );
       this.mesh.getMatrixAt(i, mat);
-      // this.mesh.getColorAt(i, dummy.material.color);
+      this.mesh.getColorAt(i, c);
+      for (let i = 0; i < geo.attributes.position.count * 3; i += 3) {
+        geo.attributes.color.array[i] = c.r;
+        geo.attributes.color.array[i + 1] = c.g;
+        geo.attributes.color.array[i + 2] = c.b;
+      }
       mat.decompose(position, quaternion, scale);
       position.y = this.mesh.geometry.attributes.height.array[i];
       geo.translate(position.x, position.y, position.z);
@@ -347,8 +362,8 @@ class HeightMap {
         if (result instanceof ArrayBuffer) {
           downloadArrayBuffer(result, `blocky-earth-.ply`);
         } else {
-          const output = JSON.stringify(result, null, 2);
-          downloadStr(output, `blocky-earth-.ply`);
+          // const output = JSON.stringify(result, null, 2);
+          downloadStr(result, `blocky-earth-.ply`);
         }
       },
       options
