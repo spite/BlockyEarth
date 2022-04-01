2b50536cc27998d819670670ccb9822b7525029a
thespite <the.spite@gmail.com>
2022-04-01 21:58:50 +0100

    Update fbo.js

--- modules/fbo.js
+++ modules/fbo.js
@@ -4,22 +4,9 @@ import {
   LinearFilter,
   RGBAFormat,
   UnsignedByteType,
-  WebGLMultisampleRenderTarget,
 } from "../third_party/three.module.js";
 
-function getFBO(w, h, options = {}, antialiased = false) {
-  if (antialiased) {
-    return new WebGLMultisampleRenderTarget(w, h, {
-      wrapS: options.wrapS || ClampToEdgeWrapping,
-      wrapT: options.wrapT || ClampToEdgeWrapping,
-      minFilter: options.minFilter || LinearFilter,
-      magFilter: options.magFilter || LinearFilter,
-      format: options.format || RGBAFormat,
-      type: options.type || UnsignedByteType,
-      stencilBuffer: options.stencilBuffer || false,
-      depthBuffer: options.depthBuffer || true,
-    });
-  }
+function getFBO(w, h, options = {}) {
   return new WebGLRenderTarget(w, h, {
     wrapS: options.wrapS || ClampToEdgeWrapping,
     wrapT: options.wrapT || ClampToEdgeWrapping,
