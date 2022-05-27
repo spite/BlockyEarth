7dfe57f38df6060d036c734327992451c4a6d3ac
thespite <the.spite@gmail.com>
2022-05-27 01:27:02 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const googleMapsKey = "AIzaSyCucPfAmxVYOmJBRVufS-RRnv3mEm0saTo";
+const openRouteKey = "5b3ce3597851110001cf62486d5e71666cbe43a4bfa7469bfe7bb042";
+const mapBoxKey =
+  "pk.eyJ1Ijoic3BpdGUiLCJhIjoiMU9EUUJlZyJ9.FXckC6N4GTPsqe1ua1u_5g";
+const nextZenKey = "rhDVUtyIRRWM3Umq0oELJw";
+
+export { googleMapsKey, openRouteKey, mapBoxKey, nextZenKey };
